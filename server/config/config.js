c3bdaa9cb9e8bbdf5b5653c672de277a8ee1c77a
Ruben Castillo <rubenx87@gmail.com>
2020-05-09 01:48:02 +0200

    errores correguidos

--- server/config/config.js
+++ server/config/config.js
@@ -10,20 +10,21 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //  base de datos
 // ============================
 
-if (process.env.NODE_ENV === 'dev') {
+/*if (process.env.NODE_ENV === 'dev') {
     process.env.URLDB = 'mongodb://localhost:27017/cafe';
 } else {
     process.env.URLDB = process.env.URLBD;
-}
+}*/
+process.env.URLDB = 'mongodb+srv://admin:admin1234@cluster0-y8drr.mongodb.net/test?retryWrites=true&w=majority';
 // ============================
 //  semilla token
 // ============================
-process.env.SEED = process.env.SEED || 'entorno-pruebas'
-    // ============================
-    // caducidad del token
-    // 60 segundos
-    // 60 minutos
-    // 24 horas
-    // 30 dias
-    // ============================
+process.env.SEED = 'entorno-desarrollo'; //process.env.SEED || 'entorno-pruebas';
+// ============================
+// caducidad del token
+// 60 segundos
+// 60 minutos
+// 24 horas
+// 30 dias
+// ============================
 process.env.TIMETOKEN = 60 * 60 * 24 * 30;
